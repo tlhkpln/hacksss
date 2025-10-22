521b799ba432d61afd1922ebcdbbe4456d7362a0
tlhkpln <152076660+tlhkpln@users.noreply.github.com>
2025-10-22 06:54:01 +0300

    Update esek.php

--- esek.php
+++ esek.php
@@ -29,7 +29,7 @@ yetkiKontrol($kullaniciAdi,$sifre);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>#tlhkpln#</title>
+    <title>#tkh#</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
@@ -313,5 +313,6 @@ yetkiKontrol($kullaniciAdi,$sifre);
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
 </body>
+
 
 </html>
